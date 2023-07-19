5326c9f51520e284d0a24962ca8b856c3faf2a75
Kasawat Chumnirat <109414047+NetStriKerX@users.noreply.github.com>
2023-07-19 13:48:33 +0700

    Update and rename spec_test.rb to test_spec.rb

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,24 +1,22 @@
 require './test.rb'
 
 RSpec.describe Test do
+
+  let(:instance) { described_class.new }
+
   it "Find the largest value obtained by permutations of digits." do
-    t = Test.new
-    expect(t.largest_val(234)).to eq(432)
+    expect(instance.largest_val(234)).to eq 432
   end
   it "Find the largest value obtained by permutations of digits." do
-    t = Test.new
-    expect(t.largest_val(454)).to eq(544)
+    expect(instance.largest_val(454)).to eq 544
   end
   it "Find the largest value obtained by permutations of digits." do
-    t = Test.new
-    expect(t.largest_val(287)).to eq(872)
+    expect(instance.largest_val(287)).to eq 872
   end
   it "Find the largest value obtained by permutations of digits." do
-    t = Test.new
-    expect(t.largest_val(295)).to eq(952)
+    expect(instance.largest_val(295)).to eq 952
   end
   it "Find the largest value obtained by permutations of digits." do
-    t = Test.new
-    expect(t.largest_val(618)).to eq(861)
+    expect(instance.largest_val(618)).to eq 861
   end
  end
